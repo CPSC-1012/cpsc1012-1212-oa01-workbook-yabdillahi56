ef05f40ef478d1f42262eec8513782af7fe3f7d3
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-10 15:46:16 -0800

    wages project

---
+++ Wages/Wages/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Wages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double regularWages;
+            double basePay = 25;
+            double regularHours = 40;
+            double overtimeWages;
+            double overtimePay = 37.5;
+            double overtimeHours = 10;
+            double totalWages;
+
+            regularWages = basePay * regularHours;
+            overtimeWages = overtimePay * overtimeHours;
+            totalWages = regularWages + overtimeWages;
+            Console.WriteLine($"Wages for this week are {totalWages:c}");
+        }
+    }
+}
