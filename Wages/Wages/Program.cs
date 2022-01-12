4f1453d456e5bfad02f9c25b4edf15815445fb82
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-12 15:36:24 -0800

    update the wages app

--- Wages/Wages/Program.cs
+++ Wages/Wages/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿/*  Purpose:    Get the user's total wage.
+ *  Input:      base pay, regular hours, overtime pay, and overtime hours
+ *  Outputs:    wage total
+ *  Algorithm:  Step 1: Ask the user for the base pay, regular hours, overtime pay, and overtime hours, then read the user's input
+ *              Step 2: Caclulate the wage total using the formula:
+ *                      regularWages = basePay * regularHours;
+                        overtimeWages = overtimePay * overtimeHours;
+                        totalWages = regularWages + overtimeWages;
+ *              Step 3: Display the computed total wage to the screen.
+ *  Last modified:  2022-01-12
+ */
+using System;
 
 namespace Wages
 {
@@ -6,14 +17,25 @@ namespace Wages
     {
         static void Main(string[] args)
         {
-            double regularWages;
-            double basePay = 25;
-            double regularHours = 40;
-            double overtimeWages;
-            double overtimePay = 37.5;
-            double overtimeHours = 10;
-            double totalWages;
+            //ask the user for input and set variables based on user input
+            Console.Write("What is the base pay: ");
+            string theuserinputstring = Console.ReadLine();
+            double basePay = double.Parse(theuserinputstring);
 
+            Console.Write("What are the regular hours: ");
+            theuserinputstring = Console.ReadLine();
+            double regularHours = double.Parse(theuserinputstring);
+
+            Console.Write("What is the overtime pay: ");
+            theuserinputstring = Console.ReadLine();
+            double overtimePay = double.Parse(theuserinputstring);
+
+            Console.Write("What are the overtime hours: ");
+            theuserinputstring = Console.ReadLine();
+            double overtimeHours = double.Parse(theuserinputstring);
+
+            //do the calculation
+            double regularWages, overtimeWages, totalWages;
             regularWages = basePay * regularHours;
             overtimeWages = overtimePay * overtimeHours;
             totalWages = regularWages + overtimeWages;
