691dfb5efe6b1e7e545f856c8a5b9b99cde68c85
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-13 14:44:58 -0800

    first excercises

---
+++ SequenceStructure/SSExercise1/SSExercise1/Program.cs
@@ -0,0 +1,31 @@
+﻿/* EXCECISE 1: Ask the user for their name, age, expected annual salary and display that to the user
+ * Date created: 2022-01-13
+ */
+
+using System;
+
+namespace SSExercise1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            int age;
+            double annualPay;
+
+            Console.Write("Enter your name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter your age: ");
+            String ageInputString = Console.ReadLine();
+            age = int.Parse(ageInputString);
+
+            Console.Write("Enter your annual salary: ");
+            String salaryInputString = Console.ReadLine();
+            annualPay = int.Parse(salaryInputString);
+
+            Console.WriteLine($"My name is {name}, my age is {age} and I hope to earn {annualPay:c} per year");
+        }
+    }
+}
---
+++ SequenceStructure/SSExercise2/SSExercise2/Program.cs
@@ -0,0 +1,34 @@
+﻿/* EXCECISE 2: Calculate the area and volume by reading the radius and length of an cylinder
+ * Date created: 2022-01-13
+ */
+using System;
+
+namespace SSExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double area;
+            double radius;
+            double pi = 3.14;
+            double volume;
+            double length;
+            String numberstring;
+
+            Console.WriteLine("Enter the radius and length of a cylinder: ");
+
+            numberstring = Console.ReadLine();
+            radius = double.Parse(numberstring);
+
+            numberstring = Console.ReadLine();
+            length = double.Parse(numberstring);
+
+            area = radius * radius * pi;
+            volume = area * length;
+
+            Console.WriteLine($"The area is {area}");
+            Console.WriteLine($"The volume is {volume}");
+        }
+    }
+}
