3d973423cc3b2701f62b1f5cb06934e957f5c7cc
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-13 15:01:05 -0800

    exercise 3

---
+++ SequenceStructure/SSExercise3/SSExercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿/* EXCECISE 3: Calculate the area of a hexagon.
+ * Date created: 2022-01-13
+ */
+using System;
+
+namespace SSExercise3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sidelength;
+            double hexagonarea;
+            String numberstring;
+
+            Console.WriteLine("Enter the length of the side:");
+            numberstring = Console.ReadLine();
+            sidelength = double.Parse(numberstring);
+
+            hexagonarea = ((3 * Math.Sqrt(3)) / 2) * (Math.Pow(sidelength, 2));
+            Console.WriteLine($"The area of the hexagon is {hexagonarea}");
+        }
+    }
+}
