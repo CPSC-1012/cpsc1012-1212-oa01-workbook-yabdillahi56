cb3148a3520f431035a79bc396173bbd5bf7a8ad
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-17 14:59:42 -0800

    the addition quiz program

---
+++ AdditionQuiz/AdditionQuiz/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AdditionQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int minnumber = 1;
+            const int maxnumber = 9;
+            Random rand = new Random();
+            int number1 = rand.Next(minnumber, maxnumber + 1);
+            int number2 = rand.Next(minnumber, maxnumber + 1);
+            int correctAnswer = number1 + number2; ;
+            Console.Write($"What is {number1} + {number2} = ? ");
+            int userAnswer = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{number1} + {number2} = {userAnswer} is {userAnswer == correctAnswer}");
+        }
+    }
+}
