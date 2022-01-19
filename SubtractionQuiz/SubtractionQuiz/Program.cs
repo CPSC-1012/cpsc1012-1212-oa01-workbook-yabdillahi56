f9bb2efc76eed77e3e09ca6eaae3e2e54b91b5d5
Yahya Abdillahi <yabdillahi1@nait.ca>
2022-01-19 15:37:47 -0800

    commit programs

---
+++ SubtractionQuiz/SubtractionQuiz/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SubtractionQuiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number1 = rand.Next(1, 10);
+            int number2 = rand.Next(1, 10);
+            if(number1 < number2)
+            {
+                int temp = number1;
+                number1 = number2;
+                number2 = temp;
+            }
+            
+        }
+    }
+}
